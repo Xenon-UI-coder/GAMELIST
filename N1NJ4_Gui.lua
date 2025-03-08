811e5f9c2d8541c953f1b8c535d4e1e6920439c1
N1NJ4 <osgc5rblx@gmail.com>
2025-03-08 20:07:40 +0700

    N1NJ4_Gui.lua

--- N1NJ4_Gui.lua
+++ N1NJ4_Gui.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [10218821851] = "Link of script", -- Client Replication
+  [10218821851] = "https://raw.githubusercontent.com/Xenon-UI-coder/N1NJ4-Project/main/N1NJ4gui_V2.lua", -- Client Replication
 }
 
 return Games
